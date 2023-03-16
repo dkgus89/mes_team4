d4e6353e123c96c9ddf5cc1bef89e37d5ee7951b
user <user@DESKTOP-2PGRMF4>
2023-03-16 09:28:24 +0900

    COPY PAGE UPDATE

--- mes_team4/src/main/java/com/itwillbs/controller/MainController.java
+++ mes_team4/src/main/java/com/itwillbs/controller/MainController.java
@@ -1,10 +1,15 @@
 package com.itwillbs.controller;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,6 +20,12 @@ import com.itwillbs.service.MemberServiceImpl;
 
 @Controller
 public class MainController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+
+		return "redirect:/Main";
+	}
 	
 	@RequestMapping(value = "/Main", method = RequestMethod.GET)
 	public String main() {
