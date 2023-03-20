7ec077fbc2b09bef2b663b12719fe3e90d6c3386
user <user@DESKTOP-2PGRMF4>
2023-03-20 17:43:35 +0900

    update 230320

---
+++ mes_team4/src/main/java/com/itwillbs/domain/PurchaseDTO.java
@@ -0,0 +1,11 @@
+package com.itwillbs.domain;
+
+import java.sql.Timestamp;
+
+public class PurchaseDTO {
+	private String purchase_cd;
+	private String business_cd;
+	private String emp_no;
+	private Timestamp date;
+
+}//class
