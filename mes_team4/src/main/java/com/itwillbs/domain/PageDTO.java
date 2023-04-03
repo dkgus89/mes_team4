6cbe5bbe4016e7a6e6b611b909d09c788dc01070
user <user@DESKTOP-2PGRMF4>
2023-04-03 09:48:17 +0900

    update 230403

--- mes_team4/src/main/java/com/itwillbs/domain/PageDTO.java
+++ mes_team4/src/main/java/com/itwillbs/domain/PageDTO.java
@@ -1,35 +1,58 @@
 package com.itwillbs.domain;
 
+import java.util.List;
+
 public class PageDTO {
-	private int pageSize;
-	private String pageNum;
-	private int currentPage;
-	private int startRow;
-	private int endRow;
-	private int pageBlock;
-	private int startPage;
-	private int endPage;
-	private int count;
-	private int pageCount;
-	// 검색어 
-	private String search;
+	// DB 쿼리문 입력 멤버변수
+	private int pageSize; // 화면에 보여줄 글의 개수
+	private int startRow; // 테이블에서 가져올 시작 글번호
+	private int endRow; // 시작 글번호에서 가져올 글 개수
+	private String search; // 페이지 검색어
+	private String product_dv; // 품목 구분
 	
+	// 페이징처리 계산 멤버변수
+	private String pageNum; // 현재 화면의 페이지번호 String
+	private int CurrentPage; // 현재 화면의 페이지번호 int(형변환)
+	private int pageBlock; // 한 화면에 나타낼 페이징 개수
+	private int pageCount; // 총 페이징 개수
+	private int startPage; // 페이징 시작 번호
+	private int endPage; // 페이징 끝 번호
+	private int count; // 게시판의 모든 행 개수
 	
+	// 테이블 병합처리 멤버변수
+	private List<Integer> rowcolsTd; // td의 rowcols 수 
+	private List<Integer> showTd; // td를 나타낼 위치
+			
+	public List<Integer> getRowcolsTd() {
+		return rowcolsTd;
+	}
+	public void setRowcolsTd(List<Integer> rowcolsTd) {
+		this.rowcolsTd = rowcolsTd;
+	}
+	public List<Integer> getShowTd() {
+		return showTd;
+	}
+	public void setShowTd(List<Integer> showTd) {
+		this.showTd = showTd;
+	}
+	public String getProduct_dv() {
+		return product_dv;
+	}
+	public void setProduct_dv(String product_dv) {
+		this.product_dv = product_dv;
+	}
 	public String getSearch() {
 		return search;
 	}
-
 	public void setSearch(String search) {
 		this.search = search;
 	}
-
-	@Override
-	public String toString() {
-		return "PageDTO [pageSize=" + pageSize + ", pageNum=" + pageNum + ", currentPage=" + currentPage + ", startRow="
-				+ startRow + ", endRow=" + endRow + ", pageBlock=" + pageBlock + ", startPage=" + startPage
-				+ ", endPage=" + endPage + ", count=" + count + ", pageCount=" + pageCount + "]";
+	public int getCount() {
+		return count;
 	}
-	
+	public void setCount(int count) {
+		this.count = count;
+	}
 	public int getPageSize() {
 		return pageSize;
 	}
@@ -43,10 +66,10 @@ public class PageDTO {
 		this.pageNum = pageNum;
 	}
 	public int getCurrentPage() {
-		return currentPage;
+		return CurrentPage;
 	}
 	public void setCurrentPage(int currentPage) {
-		this.currentPage = currentPage;
+		CurrentPage = currentPage;
 	}
 	public int getStartRow() {
 		return startRow;
@@ -78,19 +101,10 @@ public class PageDTO {
 	public void setEndPage(int endPage) {
 		this.endPage = endPage;
 	}
-	public int getCount() {
-		return count;
-	}
-	public void setCount(int count) {
-		this.count = count;
-	}
 	public int getPageCount() {
 		return pageCount;
 	}
 	public void setPageCount(int pageCount) {
 		this.pageCount = pageCount;
 	}
-	
-	
-	
-}
+}//class
